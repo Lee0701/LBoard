ea77647e73f486005c3a756c97571fdf6b177c51
Lee0701 <hgl1002@naver.com>
2019-04-01 08:56:51 +0900

    Use list view for navigation.

--- app/src/main/java/io/github/lee0701/lboard/settings/SettingsActivity.kt
+++ app/src/main/java/io/github/lee0701/lboard/settings/SettingsActivity.kt
@@ -1,15 +1,22 @@
 package io.github.lee0701.lboard.settings
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.preference.PreferenceFragmentCompat
+import android.view.LayoutInflater
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 import io.github.lee0701.lboard.R
 import kotlinx.android.synthetic.main.activity_settings.*
 
-class SettingsActivity: AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class SettingsActivity: AppCompatActivity(), AdapterView.OnItemClickListener {
+
+    val fragments = listOf<Fragment>(ActivationFragment())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,19 +28,19 @@ class SettingsActivity: AppCompatActivity(), NavigationView.OnNavigationItemSele
             setHomeAsUpIndicator(R.drawable.ic_dehaze_black_24dp)
         }
 
-        navigation.setNavigationItemSelectedListener(this)
+        val navigationList = findViewById<ListView>(R.id.navigation_list)
+        val adapter = NavigationListAdapter(this, fragments)
+        navigationList.adapter = adapter
+        navigationList.onItemClickListener = this
 
         settings.openDrawer(GravityCompat.START)
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.nav_activation -> {
-                loadFragment(ActivationFragment())
-            }
+    override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+        when(position) {
+            in 0 until fragments.size -> loadFragment(fragments[position])
         }
         settings.closeDrawer(GravityCompat.START)
-        return false
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -55,12 +62,41 @@ class SettingsActivity: AppCompatActivity(), NavigationView.OnNavigationItemSele
 
     abstract class Fragment: PreferenceFragmentCompat() {
         abstract val title: Int
+        abstract val icon: Int
     }
 
     class ActivationFragment: Fragment() {
         override val title = R.string.pref_activation_title
+        override val icon = R.drawable.ic_playlist_add_check_black_24dp
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             addPreferencesFromResource(R.xml.lboard_pref_activation)
+        }
+    }
+
+    class NavigationListAdapter(private val context: Context, private val fragments: List<Fragment>): BaseAdapter() {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            if(convertView == null) {
+                val view = View.inflate(context, R.layout.navigation_item, null)
+                view.findViewById<TextView>(R.id.item_label).setText(fragments[position].title)
+                view.findViewById<ImageView>(R.id.item_icon).setImageResource(fragments[position].icon)
+                return view
+            }
+            else {
+                return convertView
+            }
+        }
+
+        override fun getItem(position: Int): Any? {
+            return null
+        }
+
+        override fun getItemId(position: Int): Long {
+            return 0
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
         }
     }
 
