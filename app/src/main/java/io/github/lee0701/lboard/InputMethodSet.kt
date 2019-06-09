6fc0f634736dab91c5ddaaf95bef1cd9f0531c12
Lee0701 <hgl1002@naver.com>
2019-06-09 14:52:07 +0900

    Remove unused class.

--- app/src/main/java/io/github/lee0701/lboard/InputMethodSet.kt
+++
@@ -1,7 +0,0 @@
-package io.github.lee0701.lboard
-
-class InputMethodSet(
-        val keyModes: List<InputMethod>
-) {
-    constructor(normalMode: InputMethod, symMode: InputMethod): this(listOf(normalMode, symMode))
-}
