bbb36544b1dfeb4d1211e3fc6eab8b4f2280b921
Lee0701 <hgl1002@naver.com>
2019-08-28 16:35:51 +0900

    Edit bottom row of mini soft layouts.

--- app/src/main/java/io/github/lee0701/lboard/layouts/soft/MiniSoftLayout.kt
+++ app/src/main/java/io/github/lee0701/lboard/layouts/soft/MiniSoftLayout.kt
@@ -34,11 +34,12 @@ object MiniSoftLayout {
             Key(41, ""),    // m
             Key(67, "DEL", repeatable = true, keyWidth = 0.15f)
     ), Row.Type.ODD, marginLeft = 0.1f), Row(listOf(
-            Key(keyCode = 63, label = "?12", keyWidth = 1.5f/10f),
-            Key(keyCode = 204, label = "ABC", keyWidth = 1.5f/10f),
+            Key(keyCode = 63, label = "?12", keyWidth = 0.125f),
+            Key(keyCode = 55, label = ",", keyWidth = 0.1f),
+            Key(keyCode = 204, label = "ABC", keyWidth = 0.125f),
             Key(keyCode = 62, keyWidth = 4/10f),
             Key(keyCode = 56, label = ".", keyWidth = 1/10f),
-            Key(keyCode = 66, label = "RETURN", keyWidth = 2/10f)
+            Key(keyCode = 66, label = "RETURN", keyWidth = 0.15f)
     ), Row.Type.BOTTOM)), keyWidth = 1/8f, key = "mini-7cols", nameStringKey = R.string.pref_method_soft_layout_mini_7cols)
 
     val LAYOUT_MINI_7COLS = Layout(listOf(Row(listOf(
@@ -66,11 +67,12 @@ object MiniSoftLayout {
             Key(0x2035),
             Key(67, "DEL", repeatable = true)
     ), Row.Type.ODD), Row(listOf(
-            Key(keyCode = 63, label = "?12", keyWidth = 1.5f/10f),
-            Key(keyCode = 204, label = "ABC", keyWidth = 1.5f/10f),
+            Key(keyCode = 63, label = "?12", keyWidth = 0.125f),
+            Key(keyCode = 55, label = ",", keyWidth = 0.1f),
+            Key(keyCode = 204, label = "ABC", keyWidth = 0.125f),
             Key(keyCode = 62, keyWidth = 4/10f),
             Key(keyCode = 56, label = ".", keyWidth = 1/10f),
-            Key(keyCode = 66, label = "RETURN", keyWidth = 2/10f)
+            Key(keyCode = 66, label = "RETURN", keyWidth = 0.15f)
     ), Row.Type.BOTTOM)), keyWidth = 1/7f, key = "mini-8cols", nameStringKey = R.string.pref_method_soft_layout_mini_7cols)
 
 }
